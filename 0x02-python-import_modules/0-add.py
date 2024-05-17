de4f1e0d2453fbbc236a328a4f8072b2fae59f18
Abdu <Abdu>
2024-05-17 06:07:44 -0700

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-from add_0 import add
-print("{:d} + {:d} = {:d}".format(1, 2, add(1, 2)))
+def main():
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    pass
+
+
+if __name__ == '__main__':
+    main()

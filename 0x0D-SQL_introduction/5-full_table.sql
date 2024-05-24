8b08c266881065076c3cb664313059501b1619fc
Abdu <Abdu>
2024-05-24 05:42:11 -0700

    task 5

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- This script prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'first_table';

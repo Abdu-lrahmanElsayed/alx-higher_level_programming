cc64691b4eb7128d443bf4902ff98be9e5e276c8
Abdu <Abdu>
2024-05-28 04:58:53 -0700

    task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- This script creates the MySQL server user user_0d_1 password should be set to user_0d_1_pwd.
+-- This script creates the MySQL server user user_0d_1 password should be set to user_0d_1_pwd
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';

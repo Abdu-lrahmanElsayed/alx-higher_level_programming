28b61783f4c22e79216a8ccc75e8fe9dea4f1340
Abdu <Abdu>
2024-06-19 06:10:37 -0700

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-if (argv.length > 3)
-	console.log('Arguments found');
-else if (argv.length == 3)
-	console.log('Argument found');
-else
-	console.log('No argument');
-
+if (argv.length > 3) {
+  console.log('Arguments found');
+} else if (argv.length === 3) {
+  console.log('Argument found');
+} else { console.log('No argument'); }

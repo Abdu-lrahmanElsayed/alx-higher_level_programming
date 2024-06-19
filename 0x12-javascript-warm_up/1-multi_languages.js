6b107a81703bc4d8ad2532481763c408390f76c5
Abdu <Abdu>
2024-06-19 05:22:32 -0700

    task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const c = 'C is fun';
 const py = 'Python is cool';
-const js = 'avaScript is amazing';
+const js = 'JavaScript is amazing';
 console.log(c + '\n' + py + '\n' + js);

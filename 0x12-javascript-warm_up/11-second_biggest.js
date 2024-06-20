cdf87f33e55effa8497fc36a581ea89dcf567ba4
Abdu <Abdu>
2024-06-20 14:47:16 -0700

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,5 +3,5 @@ const args = process.argv.slice(2);
 const ints = args.map(arg => parseInt(arg, 10));
 const sortint = ints.sort((a, b) => b - a);
 if (sortint.length > 1) {
-  console.log(ints[0]);
+  console.log(ints[1]);
 } else { console.log(0); }

c56abc519efb414b8f8dfd62ddb7bb8ed1c88b9d
Abdu <Abdu>
2024-06-20 12:15:58 -0700

    task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-function add(a, b) {
-	return a + b;
+function add (a, b) {
+  return a + b;
 }
 const [arg1] = process.argv.slice(2);
 const [arg2] = process.argv.slice(3);
-let sum = add(+arg1, +arg2);
+const sum = add(+arg1, +arg2);
 console.log(sum);

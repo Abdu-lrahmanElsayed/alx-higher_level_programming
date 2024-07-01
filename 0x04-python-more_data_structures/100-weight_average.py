bc20c3d89c4fec8063e0ce3fec08008cb633658f
Abdu <Abdu>
2024-07-01 06:51:08 -0700

    task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,7 +2,7 @@
 def weight_average(my_list=[]):
     total_score = 0
     total_weight = 0
-    if my_list is None:
+    if my_list is None or my_list == []:
         return 0
     else:
         for s, w in my_list:

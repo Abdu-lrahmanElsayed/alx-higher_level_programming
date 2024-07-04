c7a95a0fb31a22c636c7da21bcf094a06a862371
Abdu <Abdu>
2024-07-04 11:09:45 -0700

    task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    my_list_len = 0
+    try:
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                my_list_len += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+        return my_list_len

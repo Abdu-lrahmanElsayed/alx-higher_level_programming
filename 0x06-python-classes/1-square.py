708d72fabea12b2df024e928526ac92e70e82239
Abdu <Abdu>
2024-07-10 02:42:12 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""I will create class Square with a size"""
+
+
+class Square:
+    """My class Square"""
+
+    def __init__(self, size):
+        """Initializes data
+
+            args:
+                size: Private instance attribute"""
+        self.__size = size

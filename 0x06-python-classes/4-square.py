0976e4e8b463443e5a30febeefbfb2e00ab654fd
Abdu <Abdu>
2024-07-10 04:22:15 -0700

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""I will create the same class Square with a size but with vlue and area
+    and this time with property for getter and setter"""
+
+
+class Square:
+    """my Square"""
+
+    def __init__(self, size=0):
+        """Initializes data
+
+            args:
+                size: Private instance attribute with standard value 0"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """size getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size setter
+
+            args:
+                value for size
+
+            raises:
+                TypeError: If `size` is not an integer.
+                ValueError: If `size` is less than 0."""
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """A method for erea"""
+        return self.__size ** 2

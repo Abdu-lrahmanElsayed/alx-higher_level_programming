c6c94e012d5c22f53b961148f4a30d3a84c66228
Abdu <Abdu>
2024-07-15 14:58:27 -0700

    task 2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""My reactangle class"""
+
+
+class Rectangle:
+    """A reactangle with width and height"""
+
+    def __init__(self, width=0, height=0):
+        """Initializes data
+
+            args:
+                width with standard value 0.
+                height with standard value 0.
+                """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter
+
+            raises:
+                TypeError: if width is not integer.
+                ValueError: if width is less than 0.
+                """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter
+
+            raises:
+                TypeError: if height is not integer.
+                ValueError: if height less than 0.
+                """
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        """Instance method to calculate rectangle area"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """Instance method to calculate rectangle perimeter"""
+        return (self.__height + self.__width) * 2

e7f6a91b0e473918fd9ee4557cc55b778e551403
Abdu <Abdu>
2024-07-18 12:31:28 -0700

    task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -38,3 +38,6 @@ class Square(Rectangle):
         self.__size = size
         super().__init__(size, size)
         self.integer_validator("size", size)
+
+    def area(self):
+        return self.__size ** 2

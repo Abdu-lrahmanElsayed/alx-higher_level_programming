8b60a60fa89136d3c5b3b8688ed559286884fa6c
Abdu <Abdu>
2024-07-20 10:28:39 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,7 +4,6 @@
 
 def read_file(filename=""):
     """the function"""
-    f = open(filename, 'r', encoding="UTF8")
-    for line in f:
-        print(line, end="")
-    f.close()
+    with open(filename, 'r', encoding="UTF8") as f:
+        for line in f:
+            print(line, end="")

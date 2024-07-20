c9f3becf5efafe0d1cf81ce0252c732399f1e9ed
Abdu <Abdu>
2024-07-20 12:23:49 -0700

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,6 +9,9 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
 filename = "add_item.json"
-mytxt = sys.argv
-save_to_json_file(mytxt[1:], filename)
-load_from_json_file(filename)
+if exists(filename):
+    mytxt = load_from_json_file(filename)
+else:
+    mytxt = []
+mytxt.extend(sys.argv[1:])
+save_to_json_file(mytxt, filename)

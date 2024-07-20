344aba4018b94442b813181f03a37011f9b491fd
Abdu <Abdu>
2024-07-20 11:42:19 -0700

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""a function that creates an Object from a “JSON file”"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """the function"""
+    with open(filename, 'r', encoding="UTF8") as f:
+        txt = ""
+        for line in f:
+            txt += line
+    return json.loads(txt)

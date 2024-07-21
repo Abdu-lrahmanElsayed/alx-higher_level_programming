57d674941cbf11d6a731e630b89849dcb1d5a493
Abdu <Abdu>
2024-07-21 12:47:21 -0700

    task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A module for Square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """class Square that inherits from Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize attributes"""
+        super().__init__(size, size, x, y, id=None)
+
+    def __str__(self):
+        """method should return [Square] (<id>) <x>/<y> - <size>"""
+        return "[Square] ({}) {}/{} - {}".format(
+                self.id,
+                self.x,
+                self.y,
+                self.width
+                )

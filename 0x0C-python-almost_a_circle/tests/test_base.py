db20442ab10c6a0acd443fc7b340cab62b08b9c5
Abdu <Abdu>
2024-07-22 07:14:37 -0700

    task 0

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from models.base import Base
+from models.rectangle import Rectangle
 import unittest
 
 
@@ -14,6 +15,12 @@ class TestBase(unittest.TestCase):
     def test_id(self):
         b = Base(89)
         self.assertEqual(b.id, 89)
+
+    def test_base_to_json_string(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertEqual(json_dictionary, '[{"id": 3, "width": 10, "height": 7, "x": 2, "y": 8}]')
 
 
 if __name__ == "__main__":

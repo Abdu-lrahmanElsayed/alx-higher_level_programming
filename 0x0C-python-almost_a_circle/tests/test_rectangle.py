3e35bc5805bea1d20a5d663c3ee1853bf948f41d
Abdu <Abdu>
2024-07-22 09:16:47 -0700

    task 0

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -64,48 +64,31 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r2.area(), 56)
 
     def test_rectangle_srt(self):
-        r1 = Rectangle(4, 6, 2, 1, 12)
-        self.assertEqual(r1, "[Rectangle] (12) 2/1 - 4/6")
+        self.assertEqual(str(Rectangle(4, 6, 2, 1, 12)), "[Rectangle] (12) 2/1 - 4/6")
         r2 = Rectangle(5, 5, 1)
-        self.assertEqual(r2, "[Rectangle] (1) 1/0 - 5/5")
-
-    def test_rectangle_display(self):
-        rs = Rectangle(4, 6)
-        s = """\
-####    
-####
-####
-####
-####
-####"""
-        self.assertEqual(rs.display(), None)
-        rs1 = Rectangle(3, 2, 1, 0)
-        s2 = """\
- ###
- ###"""
-        self.assertEqual(rs1.display(), s2)
+        self.assertEqual(str(r2), "[Rectangle] (6) 1/0 - 5/5")
 
     def test_rectangle_update(self):
         r = Rectangle(10, 10, 10, 10)
         r.update(89)
-        self.assertEqual(r, "[Rectangle] (89) 10/10 - 10/10")
+        self.assertEqual(str(r), "[Rectangle] (89) 10/10 - 10/10")
         r.update(89, 2)
-        self.assertEqual(r, "[Rectangle] (89) 10/10 - 2/10")
+        self.assertEqual(str(r), "[Rectangle] (89) 10/10 - 2/10")
         r.update(89, 2, 3)
-        self.assertEqual(r, "[Rectangle] (89) 10/10 - 2/3")
+        self.assertEqual(str(r), "[Rectangle] (89) 10/10 - 2/3")
         r.update(89, 2, 3, 4)
-        self.assertEqual(r, "[Rectangle] (89) 4/10 - 2/3")
+        self.assertEqual(str(r), "[Rectangle] (89) 4/10 - 2/3")
         r.update(89, 2, 3, 4, 5)
-        self.assertEqual(r, "[Rectangle] (89) 4/5 - 2/3")
+        self.assertEqual(str(r), "[Rectangle] (89) 4/5 - 2/3")
         r1 = Rectangle(10, 10, 10, 10)
         r1.update(height=1)
-        self.assertEqual(r1, "[Rectangle] (1) 10/10 - 10/1")
+        self.assertEqual(str(r1), "[Rectangle] (8) 10/10 - 10/1")
         r1.update(width=1, x=2)
-        self.assertEqual(r1, "[Rectangle] (1) 2/10 - 1/1")
+        self.assertEqual(str(r1), "[Rectangle] (8) 2/10 - 1/1")
         r1.update(y=1, width=2, x=3, id=89)
-        self.assertEqual(r1, "[Rectangle] (89) 3/1 - 2/1")
+        self.assertEqual(str(r1), "[Rectangle] (89) 3/1 - 2/1")
         r1.update(x=1, height=2, y=3, width=4)
-        self.assertEqual(r1, "[Rectangle] (89) 1/3 - 4/2")
+        self.assertEqual(str(r1), "[Rectangle] (89) 1/3 - 4/2")
 
 
 if __name__ == "__main__":

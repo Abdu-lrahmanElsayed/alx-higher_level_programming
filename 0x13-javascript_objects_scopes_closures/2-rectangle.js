141646bb8414e542ea0d3d488ec45e796f0f7563
Abdu <Abdu>
2024-07-28 06:28:45 -0700

    task 2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0) {
+	    this.width;
+	    this.height;
+    } else if (h <= 0) {
+	    this.width;
+	    this.height;
+    } else {
+    	this.width = w;
+    	this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;

0087793a3029c0aecfe3b4ef5d453af710c71e40
Abdu <Abdu>
2024-07-29 08:59:14 -0700

    task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,8 +2,8 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || w == null || h <= 0 || h == null) {
-      this.width = undefined;
-      this.height = undefined;
+      this.width;
+      this.height;
     } else {
       this.width = w;
       this.height = h;

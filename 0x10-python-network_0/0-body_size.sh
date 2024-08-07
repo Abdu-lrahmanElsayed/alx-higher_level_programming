5d54a2d0d1cbbf4fc718f3e03ba094375a21f974
Abdu <Abdu>
2024-08-07 09:10:11 -0700

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-URL=$0
-
-size=$(curl -s -o /dev/null -w '%{size_download}' "$URL")
-
+size=$(curl -s -o /dev/null -w '%{size_download}' $0)
 echo $size

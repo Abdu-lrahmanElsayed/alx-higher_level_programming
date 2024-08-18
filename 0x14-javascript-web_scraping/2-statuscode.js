57ea8efab79f9b98b3f6b793b1ef83eae3933f9e
Abdu <Abdu>
2024-08-18 05:58:15 -0700

    task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+
+function displayStatusCode (url) {
+  request(url, (error, response) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log('code: ' + response.statusCode);
+    }
+  });
+}
+
+const url = process.argv[2];
+displayStatusCode(url);
